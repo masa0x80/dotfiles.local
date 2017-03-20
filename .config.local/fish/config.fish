14bc3f38738275744f374908d6ec4b48a1d26430
Kimura Masayuki <masa0x80@gmail.com>
2017-03-20 21:45:08 +0900

    [fish] Remove `set -U`

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -1,4 +1,4 @@
 if type -qa ghq
-  set -x DOTFILE (ghq root)/github.com/masa0x80/dotfiles
-  set -U fish_user_paths (ghq root)/github.com/masa0x80/dotfiles.local/bin $fish_user_paths
+  set -gx DOTFILE (ghq root)/github.com/masa0x80/dotfiles
+  set -gx fish_user_paths (ghq root)/github.com/masa0x80/dotfiles.local/bin $fish_user_paths
 end
