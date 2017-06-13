806993399e6ec30906ac329fad2dba78af34ad98
Kimura Masayuki <masa0x80@gmail.com>
2017-06-13 12:43:44 +0900

    [mitamae] Fix versions

--- etc/mitamae/attributes/versions.rb
+++ etc/mitamae/attributes/versions.rb
@@ -1,4 +1,10 @@
 node.reverse_merge!(
+  git: {
+    version: '2.13.0'
+  },
+  golang: {
+    version: '1.8'
+  },
   remi_repo: {
     rpm_url: 'http://rpms.famillecollet.com/enterprise/remi-release-7.rpm',
     package: 'remi-release'
