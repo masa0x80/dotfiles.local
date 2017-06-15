90d4f54f5ad1224683723edb458e6dadc3ef6927
Kimura Masayuki <masa0x80@gmail.com>
2017-06-16 00:33:57 +0900

    Add DOTFILE_ROLE environment variable

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -1,3 +1,6 @@
+# When executing `make init` in the dotfiles directory, both common and append roles are done.
+set -gx DOTFILE_ROLE 'common,append'
+
 if type -qa ghq
   set -gx DOTFILE (ghq root)/github.com/masa0x80/dotfiles
   set -gx fish_user_paths (ghq root)/github.com/masa0x80/dotfiles.local/bin $fish_user_paths
