84d050f4c551cda9347f80c2791d548bd7e338b2
Kimura Masayuki <masa0x80@gmail.com>
2017-06-28 20:37:37 +0900

    $ fish_indent -w .config.local/fish/config.fish

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -2,7 +2,7 @@
 set -gx DOTFILE_ROLE 'common,append'
 
 if type -qa ghq
-  set -gx DOTFILE (ghq root)/github.com/masa0x80/dotfiles
-  set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
-  set -gx fish_user_paths $DOTFILE_LOCAL/bin $fish_user_paths
+    set -gx DOTFILE (ghq root)/github.com/masa0x80/dotfiles
+    set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
+    set -gx fish_user_paths $DOTFILE_LOCAL/bin $fish_user_paths
 end
