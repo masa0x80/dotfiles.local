ecbfc0438caa1055d89fcaf4c722caae603a713d
Kimura Masayuki <masa0x80@gmail.com>
2017-07-05 01:53:30 +0900

    [golang] Add golang dev tools

--- etc/mitamae/attributes/golang_repos.rb
+++ etc/mitamae/attributes/golang_repos.rb
@@ -1,5 +1,9 @@
 node.reverse_merge!(
   golang_repos: %w(
+    golang.org/x/tools/cmd/...
+    github.com/golang/lint/golint
+    github.com/Masterminds/glide
+    github.com/motemen/gore
     github.com/masa0x80/mdv/...
   )
 )
