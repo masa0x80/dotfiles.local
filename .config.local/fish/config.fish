30b7c9894f07882b413e08b57cd51af8f4ff0bf7
KIMURA Masayuki <masa0x80@gmail.com>
2018-01-06 15:41:50 +0900

    Specify `$HUB_CONFIG`

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -1,6 +1,9 @@
 # When executing `make init` in the dotfiles directory, both common and append roles are done.
 set -gx DOTFILE_ROLE 'common,append'
 
+# hub
+set -gx HUB_CONFIG $HOME/.config/hub/config
+
 if type -qa ghq
     set -gx DOTFILE (ghq root)/github.com/masa0x80/dotfiles
     set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
