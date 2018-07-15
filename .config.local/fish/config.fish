891243f25570ee22d0661442a06ee5c4724f9d9a
KIMURA Masayuki <masa0x80@gmail.com>
2018-07-16 00:59:20 +0900

    Tweaks

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -11,3 +11,5 @@ if type -qa ghq
     set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
     set -gx fish_user_paths $DOTFILE_LOCAL/bin $fish_user_paths
 end
+
+__load_file $HOME/Box/.config/config.fish
