ec1c963108cc56c5c0750cd795741c931e63aac4
KIMURA Masayuki <masa0x80@gmail.com>
2019-05-15 02:16:35 +0900

    Tweaks

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -7,9 +7,9 @@ set -gx HUB_CONFIG $HOME/.config/hub/config
 set -gx DISABLE_ISSUE_NUMBER 1
 
 if type -qa ghq
-    set -gx DOTFILE (ghq root)/github.com/masa0x80/dotfiles
-    set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
-    set -gx fish_user_paths $fish_user_paths $DOTFILE_LOCAL/bin
+    set -x DOTFILE (ghq root)/github.com/masa0x80/dotfiles
+    set -x DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
+    set -gx PATH $DOTFILE_LOCAL/bin $PATH
 end
 
 __load_file $HOME/Box/.config/config.fish
