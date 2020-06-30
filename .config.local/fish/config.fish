7cf583c5d9f2482fc78adc8bda113fca51ef135f
KIMURA Masayuki <masa0x80@gmail.com>
2020-07-01 00:33:34 +0900

    [fish] Fix config

--- .config.local/fish/config.fish
+++ .config.local/fish/config.fish
@@ -1,16 +1,14 @@
-# When executing `make init` in the dotfiles directory, both common and append roles are done.
-set -gx DOTFILE_ROLE 'common'
-
 # hub
 set -gx HUB_CONFIG $HOME/.config/hub/config
 
 set -gx DISABLE_ISSUE_NUMBER 1
 
-if type -qa ghq
-    set -x DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
-    __add_fish_user_paths $DOTFILE_LOCAL/bin
-    set -x DOTFILE (ghq root)/github.com/masa0x80/dotfiles
-    __add_fish_user_paths $DOTFILE/bin
-end
+type -qa ghq && set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
 
 __load_file $HOME/Box/.config/config.fish
+
+# Append $DOTFILE_LOCAL/bin to $PATH
+if not set -q DOTFILE_LOCAL
+    type -qa ghq && set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
+end
+test -d $DOTFILE_LOCAL/bin && __add_fish_user_paths $DOTFILE_LOCAL/bin
