ffae0d241d0531ca67e81598a40e7a3d2d085cc5
KIMURA Masayuki <masa0x80@gmail.com>
2021-02-14 21:43:31 +0900

    Tweaks

--- .config.local/fish/login.fish
+++ .config.local/fish/login.fish
@@ -12,3 +12,11 @@ if not set -q DOTFILE_LOCAL
     type -qa ghq && set -gx DOTFILE_LOCAL (ghq root)/github.com/masa0x80/dotfiles.local
 end
 test -d $DOTFILE_LOCAL/bin && __add_fish_user_paths $DOTFILE_LOCAL/bin
+
+# Snippets
+set -l snippets_path (ghq root)/github.com/masa0x80/snippets/snippets.d
+if test "$snippets_root" = ''
+    set -gx snippets_root $snippets_path
+else
+    set -a snippets_root $snippets_path
+end
