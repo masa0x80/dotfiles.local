0175acb2ac6208efe5c6dac3e66c99f7c4e2aeb3
KIMURA Masayuki <masa0x80@gmail.com>
2024-01-30 23:50:03 +0900

    [nvim] ✏️ fix: obsidian config

--- .config.local/nvim/lua/plugins/conf/obsidian.lua
+++ .config.local/nvim/lua/plugins/conf/obsidian.lua
@@ -32,6 +32,7 @@ require("obsidian").setup({
 			[">"] = { char = "", hl_group = "ObsidianRightArrow" },
 			["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
 		},
+		bullets = {},
 		hl_groups = {
 			ObsidianTodo = { bold = true, fg = c.orange },
 			ObsidianDone = { bold = true, fg = c.cyan },
