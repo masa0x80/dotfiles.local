cb1b4bd41beb34d288adf7e495adef6fedea259e
KIMURA Masayuki <masa0x80@gmail.com>
2024-04-12 23:23:09 +0900

    [finicky] 🎸 feat: add config

---
+++ .finicky.js
@@ -0,0 +1,13 @@
+module.exports = {
+  defaultBrowser: "Vivaldi",
+  handlers: [
+    {
+      match: /figma\.com\/file\//,
+      browser: "Figma",
+    },
+    {
+      match: /zoom\.us\/j\//,
+      browser: "us.zoom.xos",
+    },
+  ],
+};
